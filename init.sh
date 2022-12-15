1ba01403aba9b6e79aee95884bcc4fbabb4f0985
BwayCer <bwaycer@gmail.com>
2022-12-15 12:33:55 +0800

    feat: 更新自動初始化腳本

--- init.sh
+++ init.sh
@@ -6,14 +6,29 @@ __filename=`realpath "$0"`
 _dirsh=`dirname "$__filename"`
 
 
-tmpPlugPath="$_dirsh/autoload/plug.vim"
-[ -f "$tmpPlugPath" ] \
-    || curl -s "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" \
-        > "$tmpPlugPath"
+plugPath="$_dirsh/localShare/site/autoload/plug.vim"
+[ -f "$plugPath" ] \
+  || curl -L "https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim" \
+      --create-dirs -o "$plugPath"
 
-tmpOriginVimrcPath="$HOME/.vimrc"
-tmpVimrcPath="$_dirsh/.vimrc"
-[ -f "$tmpOriginVimrcPath" ] \
-    && [ "`realpath "$tmpOriginVimrcPath"`" != "$tmpVimrcPath" ] \
-    || ln -sf "$tmpVimrcPath" "$tmpOriginVimrcPath"
+# when use vim
+if type vim > /dev/null; then
+  vimrcPath="$_dirsh/.vimrc"
+  vimrcForVimPath="$HOME/.vimrc"
+  if [ ! -f "$vimrcForVimPath" ]; then
+    ln -sf "$vimrcPath" "$vimrcForVimPath"
+  elif [ "`realpath "$vimrcForVimPath"`" != "$vimrcPath" ]; then
+    echo "Pleace remove \"$vimrcForVimPath\""
+  fi
+fi
+
+# when use neovim
+if type nvim > /dev/null; then
+  vimrcForNvimPath="$HOME/.config/nvim"
+  if [ ! -e "$vimrcForNvimPath" ]; then
+    ln -sf "$_dirsh" "$vimrcForNvimPath"
+  elif [ "`realpath "$vimrcForNvimPath"`" != "$_dirsh" ]; then
+    echo "Pleace remove \"$vimrcForNvimPath\""
+  fi
+fi
 
