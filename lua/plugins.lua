a37ae61f622996ee6c0a39eaf3c006787f268497
BwayCer <bwaycer@gmail.com>
2023-05-04 18:45:31 +0800

    temp: lua

---
+++ lua/plugins.lua
@@ -0,0 +1,10 @@
+print("Hello, plugins.lua!")
+
+vim.cmd [[packadd packer.nvim]]
+
+return require('packer').startup(function(use)
+  -- Packer can manage itself
+  use 'wbthomason/packer.nvim'
+
+  -- You add plugins here
+end)
